2d1316f565ad5a8ad743cb39df4cffdc25effef9
Alexis Tan <alexis@sen-dev.com>
2017-11-10 20:22:11 +0100

    Make the bot connect to Discord via JDA

--- src/main/java/com/senither/wheredoesitgodoe/WhereDoesItGoDoe.java
+++ src/main/java/com/senither/wheredoesitgodoe/WhereDoesItGoDoe.java
@@ -1,12 +1,55 @@
 package com.senither.wheredoesitgodoe;
 
-public class WhereDoesItGoDoe {
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.MessageUpdateEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.requests.SessionReconnectQueue;
 
-    public WhereDoesItGoDoe() {
-        //
+import javax.security.auth.login.LoginException;
+
+public class WhereDoesItGoDoe extends ListenerAdapter {
+
+    private final JDA jda;
+
+    public WhereDoesItGoDoe(JDA jda) {
+        this.jda = jda;
+        this.jda.addEventListener(this);
     }
 
-    public static void main(String[] args) {
-        //
+    public static void main(String[] args) throws LoginException, RateLimitedException {
+        if (args.length == 0) {
+            System.err.println("Missing argument \"bot token\", this is required for the bot to work!");
+            System.err.println("java -jar WhereDoesItGoDoe.jar <bot token>");
+            System.exit(20);
+        }
+
+        new WhereDoesItGoDoe(new JDABuilder(AccountType.BOT)
+                .setReconnectQueue(new SessionReconnectQueue())
+                .setAutoReconnect(true)
+                .setToken(args[0])
+                .buildAsync()
+        );
+    }
+
+    private void onMessageEvent(Message message) {
+        if (!message.getChannelType().isGuild()) {
+            return;
+        }
+        System.out.println(message.getContent());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        onMessageEvent(event.getMessage());
+    }
+
+    @Override
+    public void onMessageUpdate(MessageUpdateEvent event) {
+        onMessageEvent(event.getMessage());
     }
 }
